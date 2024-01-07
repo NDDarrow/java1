a82fb761648d6e77667d4136456c53d66bda4b8c
NDDarrow <shwo5158@naver.com>
2024-01-07 18:02:51 +0900

    0602

--- java_collection/src/cafe/Order.java
+++ java_collection/src/cafe/Order.java
@@ -1,15 +1,28 @@
 package cafe;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class Order {
 	// 전체 메뉴 보여주는 메소드 - 분류(커피, 음료, 빽스치노) 선택 -> 해당 분류에서 제품 이름 입력하여 주문
 	// 커피메뉴를 선택하여 보다가 음료메뉴를 보고싶으면 이동하여 보이게 해줘야 한다.
+	
+	
 	public static void kiosk() {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("\n==========주문==========\n");
 		
 		Category main =  Category.커피;
 		while(true) {
-		System.out.println("1.커피  2.음료  3.빽스치노");
-		
+			System.out.println("1.커피  2.음료  3.빽스치노");
+			String sel = scan.nextLine();
+			file menu_list = new file();
+			
+			System.out.println();
+			if(sel == "1" || sel == "커피" ) {
+				
+			}
 		
 		}
 	}
--- java_collection/src/cafe/Order_Counter.java
+++ java_collection/src/cafe/Order_Counter.java
@@ -24,7 +24,9 @@ public class Order_Counter {
 		// 스토리 - 회원이 로그인하여 주문(모바일 또는 pc) 한다.
 		// 로그인부터 주문 과정까지를 제작
 		
-		user = login();
+//		user = login();
+		Order.kiosk();
+		
 		//주문
 	}
 	public static HashMap<Category, Set> classTask(){
@@ -32,6 +34,8 @@ public class Order_Counter {
 		Set<menu> 커피 = new HashSet<>();
 		Set<menu> 음료 = new HashSet<>();
 		Set<menu> 빽스치노 =  new HashSet<>();
+		
+		
 		
 		
 		return null;
@@ -44,12 +48,14 @@ public class Order_Counter {
 		String id = scan.nextLine();
 		System.out.print("비밀번호 : ");
 		String pw = scan.nextLine();
-		
 		member temp = new member(id,pw);
 		int idx = 0;
+		Order odr = new Order();
 		if( (idx = buyer.indexOf( temp )) != -1 ) { //아이디와 비밀번호가 일치하면
 			System.out.println("로그인 성공");
+			odr.kiosk();
 			return buyer.get( idx );
+			
 		}
 			// 아이디 또는 비밀번호가 맞지 않으면
 			System.out.println("로그인 실패");
@@ -60,5 +66,6 @@ public class Order_Counter {
 		// 반복문을 통해 객체 하나씩 가져와서 값 비교를 해준다.
 		// 또는 ArrayList에 저장된 객체의 클래스에 비교 하기 위한 관련 메서드를 구현한다 .(equals)
 	}
+	
 	
 }
