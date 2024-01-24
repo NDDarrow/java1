470e8759e82772ff1892d07487158fdba7874e2c
LEE-JI-HO <wlgh0818@naver.com>
2024-01-24 17:31:49 +0900

    0531

--- web2/src/main/java/control/member_control.java
+++ web2/src/main/java/control/member_control.java
@@ -1,6 +1,10 @@
 package control;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -10,15 +14,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.member_action;
 import service.member_service;
 
 /**
  * Servlet implementation class member_control
  */
-@WebServlet("/members")
+@WebServlet("/member_control")
 public class member_control extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private member_service ms = new member_service();
+	private Map<String, member_action> target = new HashMap<>();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -32,7 +37,20 @@ public class member_control extends HttpServlet {
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		ResourceBundle rb = ResourceBundle.getBundle("class_bundle/member");
+		Enumeration key = rb.getKeys();
+		while( key.hasMoreElements() ) {
+			String k = (String)key.nextElement(); // singup, signupSave 등등 키값 저장
+			String value = rb.getString(k); // k의 value가져오기
+			try {
+				Class<?> hc1 = Class.forName(value); //클래스를 로드(객체생성하려면 클래스가 메모리에 로드 되어야 한다.);
+				member_action hinst = (member_action)hc1.newInstance(); // 객체생성
+				target.put(k, hinst);
+			}catch(Exception e) {
+				System.out.println("member.properties 파일 작업 실패");
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
@@ -63,18 +81,16 @@ public class member_control extends HttpServlet {
 		
 		request.setCharacterEncoding("UTF-8");
 		
-		String cmd = request.getParameter("cmd");
+		String uri = request.getRequestURI();
+		
+		
+		
+		String cmd = uri.substring(9);
 		String view = "/"; // 사용자에게 제공할 기본 페이지
-		if(cmd.equals("signup") ) { //요청 파라미터가 signup일 경우 회원가입 페이지를 요청
-			request.setAttribute("prt", "/member/signup");
-			
-			request.setAttribute("emailList", ms.emailList() );
-			// 회원가입 페이지를 제공하기위해 request객체를 이용한 것 일뿐 반드시 이와같은 방법일 필요는 없음
-			// 사용자 요청에 의한 페이지 제공은 정적페이지(.html, .jsp 등) 그대로 제공할 수도 있고
-			// 위와 같이 include 방식으로 제공 할 수 있다.
-		}else if( cmd.equals("signSave") ) {
-			view = ms.insert(request, response);
-		}
+		
+		member_action mem = target.get(cmd);
+		
+		view = mem.action(request, response);
 		
 		if( view != null) {
 			RequestDispatcher rds = request.getRequestDispatcher(view); //제공할 페이지
@@ -114,3 +130,20 @@ public class member_control extends HttpServlet {
  *  VO - value object( 데이터 저장 객체 - 읽기전용)
  *  Entity - 테이블 생성과 데이블 값 저장 , 읽기, 수정, 삭제와 관련있는 클래스
  */
+
+
+
+//
+//if(cmd.equals("signup") ) { //요청 파라미터가 signup일 경우 회원가입 페이지를 요청
+//	request.setAttribute("prt", "/member/signup");
+//	
+//	request.setAttribute("emailList", ms.emailList() );
+//	// 회원가입 페이지를 제공하기위해 request객체를 이용한 것 일뿐 반드시 이와같은 방법일 필요는 없음
+//	// 사용자 요청에 의한 페이지 제공은 정적페이지(.html, .jsp 등) 그대로 제공할 수도 있고
+//	// 위와 같이 include 방식으로 제공 할 수 있다.
+//}else if( cmd.equals("signSave") ) {
+//	view = ms.insert(request, response);
+//}
+
+
+
--- web2/src/main/java/service/member_service.java
+++ web2/src/main/java/service/member_service.java
@@ -8,26 +8,31 @@ import javax.servlet.http.HttpServletResponse;
 import DAO.member_dao;
 import DTO.member;
 
-public class member_service {
+public class member_service implements member_action {
 	private member_dao mdao = new member_dao();
 	
 	public String[] emailList() {
 		return mdao.findAllEmail();
 	}
 	
-	
-	public String insert(HttpServletRequest request, HttpServletResponse response) throws IOException  {
-		
-		String email = request.getParameter("email");
-		String pw  = request.getParameter("pin");
-		String name = request.getParameter("name");
-		String tel = request.getParameter("tel");
-		
-		mdao.insert( new member(email, pw, name, tel) );
-		
-		response.sendRedirect("/members?cmd=signIn");
-		
-		return null;
+	@Override
+	public String action(HttpServletRequest request, HttpServletResponse response) throws IOException  {
+		if( request.getParameter("cmd") == null ) {
+			request.setAttribute("prt", "member/signup");
+			request.setAttribute("emailList", emailList() );
+			return "/";
+		}else {
+			String email = request.getParameter("email");
+			String pw  = request.getParameter("pin");
+			String name = request.getParameter("name");
+			String tel = request.getParameter("tel");
+			
+			mdao.insert( new member(email, pw, name, tel) );
+			
+			response.sendRedirect("/members/signIn");
+			
+			return null;
+		}
 	}
 	
 	
